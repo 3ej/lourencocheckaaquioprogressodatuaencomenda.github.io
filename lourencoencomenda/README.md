f9f676e18cb415d9a5946aa7e0be7d446cda09ce
Inês Carvalho Duarte <51207713+3ej@users.noreply.github.com>
2024-10-23 00:35:43 +0100

    Add files via upload

---
+++ lourencoencomenda/README.md
@@ -0,0 +1,1 @@
+# lourencocheckaaquioprogressodatuaencomenda.github.io
---
+++ lourencoencomenda/script.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").onclick = function() {
+    document.getElementById("response").innerText = "Hello! Thanks for clicking the button!";
+};
